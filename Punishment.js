480df55e60f39b34cb4caf4ca96f71aa98f71bd2
Nagyungmin <33858743+Nagyungmin@users.noreply.github.com>
2018-12-18 19:39:12 +0900

    벌칙 무브 조정

--- Punishment.js
+++ Punishment.js
@@ -1,40 +1,39 @@
 var Punishment = new (function () {
   var _this = this;
 
+// 말판의 정보를 저장한 배열. 술을 마시는 칸과 황금열쇠 칸, 순간이동 칸을 type으로 구분.
   var MAP_DATA = [
-  { type : "default", title : "출발", alcohol : 0},
+  { type : "none", title : "시작", alcohol : 0},
   { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "소주 1 병", alcohol : 7.5},
-  { type : "key", title : "소주 1 잔", alcohol : 1},
+  { type : "warp", title : "warp", alcohol : 0},
+  { type : "default", title : "맥주 1 잔 원샷", alcohol : 1.1},
   { type : "warp", title : "워프", alcohol : 0},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "한 번 쉬기", alcohol : 0},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "default", title : "맥주 1 잔", alcohol : 1.1},
-  { type : "default", title : "소주 1 잔", alcohol : 1},
-  { type : "warp", title : "워프", alcohol : 0}
+  { type : "key", title : "황금열쇠", alcohol : 0},
+  { type : "default", title : "맥주 1 잔 원샷", alcohol : 1.1},
+  { type : "none", title : "일어나서 노래 부르기", alcohol : 0},
+  { type : "key", title : "황금열쇠", alcohol : 0},
+  { type : "default", title : "맥주 반병 원샷", alcohol : 1.375},
+  { type : "default", title : "소주 1 잔 원샷", alcohol : 1},
+  { type : "default", title : "소주 반병 원샷", alcohol : 3.75},
+  { type : "key", title : "황금열쇠", alcohol : 0},
+  { type : "default", title : "맥주 1 잔 원샷", alcohol : 1.1},
+  { type : "warp", title : "워프", alcohol : 0},
+  { type : "none", title : "공중제비 돌기", alcohol : 0},
+  { type : "default", title : "소주 3 잔 원샷", alcohol : 3},
+  { type : "none", title : "쉬어가기", alcohol : 0},
+  { type : "default", title : "소주 한병 원샷", alcohol : 7.5},
+  { type : "key", title : "황금열쇠", alcohol : 0},
+  { type : "default", title : "맥주 2 잔 원샷", alcohol : 2.2}
   ];
 
-// 콘솔에서 벌칙과 내역 출력 메소드. 플레이어 무브에서 케이스를 쓴다면 지워도 될듯.
-  this.showCurPunish = function (i, curPlayer) {
-    var punishment = this.punishmentList["pun"+i];
-    console.log(punishment);
-    curPlayer.punishmentList.push(punishment)
-  }
-
+// 도착한 셀 정보 받아오기
   this.getCell = function (index) {
     return MAP_DATA[index];
   }
 
+/* 혈중 알콜농도 구하는 메소드. 위드마크 계산법으로 계산함.
+*  마신 술의 양을 모두 소주양으로 환산하고, 체중은 70KG과 가중치는 0.6 상수로 고정함.
+*/
   this.getBloodAlcohol = function (alcohol) {
     var cal = (alcohol*48*0.017*0.7894)/(70*0.6);
     return cal.toFixed(2);
